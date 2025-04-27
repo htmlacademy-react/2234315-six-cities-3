b488b38f3607de8c9e12f5c6e9191c581c1c6fa4
katya-illarionova <katya.illarionova@travelline.ru>
2025-04-28 02:13:27 +0300

    Добавит маршрутизацию

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,10 @@
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
+import {AppRoute} from '../../const';
 import Home from '../../pages/home/home';
+import Login from '../../pages/login/login';
+import Favorites from '../../pages/favorites/favorites';
+import Offer from '../../pages/offer/offer';
+import NotFound from '../../pages/not-found/not-found';
 
 type AppProps = {
   numberOfPlacements: number;
@@ -6,7 +12,30 @@ type AppProps = {
 
 function App({numberOfPlacements}: AppProps): JSX.Element {
   return (
-    <Home numberOfPlacements={numberOfPlacements} />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<Home numberOfPlacements={numberOfPlacements} />}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<Login />}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={<Favorites />}
+        />
+        <Route
+          path={AppRoute.Offer}
+          element={<Offer />}
+        />
+        <Route
+          path="*"
+          element={<NotFound />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/const.ts
+++ src/const.ts
@@ -1,3 +1,10 @@
 export const HomePageData = {
   NumberOfPlacements: 312
 };
+
+export enum AppRoute {
+  Main = '/',
+  Login = '/login',
+  Favorites = '/favorites',
+  Offer = '/offer/:id'
+}
