92dc8307107a252beba6c07fa48c845b0bad2523
katya-illarionova <katya.illarionova@travelline.ru>
2025-05-22 22:08:23 +0300

    Реализует загрузку и отрисовку списка предложений с сервера

---
+++ src/components/loader/loader.tsx
@@ -0,0 +1,11 @@
+import './loader.css';
+
+function Loader(): JSX.Element {
+  return (
+    <div className="loader-wrapper">
+      <div className="loader"></div>
+    </div>
+  );
+}
+
+export default Loader;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
 
 import App from './components/app/app';
-import { store } from './store';
 
+import { store } from './store';
+import { fetchOffersAction, checkAuthAction } from './store/api-actions';
 import { favoriteOffers } from './mocks/offers';
+
+import 'react-toastify/dist/ReactToastify.css';
+
+store.dispatch(fetchOffersAction());
+store.dispatch(checkAuthAction());
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -14,6 +21,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <ToastContainer />
       <App
         favoriteOffers={favoriteOffers}
       />
